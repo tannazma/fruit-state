e6ec608f270c0d23bb8e546c5ac708b96531bb08
tannazma <97254921+tannazma@users.noreply.github.com>
2023-11-16 00:37:03 +0100

    create input box to search based on the fruit names

--- src/pages/fruit-page.tsx
+++ src/pages/fruit-page.tsx
@@ -7,6 +7,8 @@ import { useState } from "react";
 // Create a component that displays a list of fruits with a button next to each fruit. When the button is clicked, the availability of the fruit should be toggled.
 const FruitList = () => {
   const [getFruitList, setFruitList] = useState<Fruit[]>(fruits);
+  const [getSearchText, setSearchText] = useState("");
+  //   console.log(getSearchText);
 
   const toggleAvailability = (id: number) => {
     // console.log(id);
@@ -17,20 +19,28 @@ const FruitList = () => {
     );
   };
 
-//   console.log(getFruitList);
+  const inputSearch = (event: React.FormEvent<HTMLInputElement>) => {
+    // console.log(event.target.value);
+    setSearchText((event.target as HTMLInputElement).value);
+  };
+
+  console.log(getFruitList);
 
   return (
     <>
+      <input value={getSearchText} onInput={inputSearch} />
       <ul>
-        {getFruitList.map((fruit) => (
-          <li key={fruit.id}>
-            {fruit.name}
-            <p>{fruit.isAvailable ? "✅" : "❌"}</p>
-            <button onClick={() => toggleAvailability(fruit.id)}>
-              Clear Fruit List
-            </button>
-          </li>
-        ))}
+        {getFruitList
+          .filter((fruit) => fruit.name.includes(getSearchText))
+          .map((fruit) => (
+            <li key={fruit.id}>
+              {fruit.name}
+              <p>{fruit.isAvailable ? "✅" : "❌"}</p>
+              <button onClick={() => toggleAvailability(fruit.id)}>
+                Clear Fruit List
+              </button>
+            </li>
+          ))}
       </ul>
     </>
   );
